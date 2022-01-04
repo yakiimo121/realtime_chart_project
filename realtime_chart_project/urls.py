e4c9ff3b63dba73e7c8ed2f24722f0c13a23055a
takuto <takuto@ucl.nuee.nagoya-u.ac.jp>
2022-01-04 17:50:03 +0900

    Refactor: url

--- realtime_chart_project/urls.py
+++ realtime_chart_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('realtime_chart/', include('realtime_chart.urls')),
+    path('realtimechart/', include('realtime_chart.urls')),
     path('admin/', admin.site.urls),
 ]
